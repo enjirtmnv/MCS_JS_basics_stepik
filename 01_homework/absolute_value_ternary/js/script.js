87d974c404441c47d8f80810939dad6d1a487f3a
enjirtmnv <akijda@mail.ru>
2018-08-22 17:49:52 +0300

    absolute value ternary

---
+++ 01_homework/absolute_value_ternary/js/script.js
@@ -0,0 +1,19 @@
+console.log('You are at ' + window.location);
+
+
+let Value = prompt('VVedite lyuboe chislo');
+let absoluteValue = Number(Value);
+//console.log(typeof absoluteValue);
+
+
+(absoluteValue >= 0) ? console.log(absoluteValue): console.log(absoluteValue - absoluteValue * 2);
+
+
+
+//if (absoluteValue == 0){
+//    console.log(0);
+//}else if (absoluteValue < 0){
+//    console.log(absoluteValue - absoluteValue * 2);
+//}else if(absoluteValue > 0){
+//    console.log(absoluteValue);
+//}
