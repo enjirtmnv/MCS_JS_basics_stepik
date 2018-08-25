7e06eda75b29f34acc8a59893c97136defa58da6
enjirtmnv <akijda@mail.ru>
2018-08-25 10:21:40 +0300

    fix arrow function bread apple money

--- 01_homework/arrow_function_bread_apple_money/js/script.js
+++ 01_homework/arrow_function_bread_apple_money/js/script.js
@@ -1,22 +1,5 @@
 console.log('You are at ' + window.location);
 
-//
-
-/*let money = window.prompt('Сколько у вас с собой есть денег?');
-let apple = window.prompt('Сколько вы купили яблок?');
-let bread = window.prompt('Сколько вы купили батонов хлеба?');
-let applePriceForOne = window.prompt('Сколько стоит одно яблоко?');
-let appleTotalCost = parseFloat(applePriceForOne) * parseFloat(apple);
-let breadPriceForOne = window.prompt('Сколько стоит один батон хлеба?');
-let breadTotalCost = parseFloat(breadPriceForOne) * parseFloat(bread);
-let busket = parseFloat(money) - (breadTotalCost + appleTotalCost);
-console.log(money, appleTotalCost, breadTotalCost, busket);
-let booleanAnswer = (busket <= money && busket >= 0);
-console.log(booleanAnswer);
-
-document.body.innerHTML = booleanAnswer;
-*/
-
 
 let haveEnough = (money, apple, bread, applePriceForOne, breadPriceForOne) => {
     let appleTotalCost = parseFloat(applePriceForOne) * parseFloat(apple);
