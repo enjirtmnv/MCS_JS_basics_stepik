627ff51d6db093faaf16e5dda57b8c26c902e3e0
enjirtmnv <akijda@mail.ru>
2018-09-06 11:36:02 +0300

    math

---
+++ 18_math/js/script.js
@@ -0,0 +1,15 @@
+
+
+function modulSquare(num) {
+    return Math.abs(num)/2
+};
+console.log(modulSquare(-3));
+console.log(modulSquare(-34));
+
+console.log(Math.floor(2.678));
+console.log(Math.round(2.678));
+
+console.log(Math.random());
+
+console.log(Math.random()*(10-5)+5);
+console.log(Math.floor(Math.random()*(10-5)+5));
